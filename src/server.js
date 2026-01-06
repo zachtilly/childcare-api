e1381f29f9ffd8cd18ffc52799515fd2c1959c14
Tilly <ztilly@strength.org>
2026-01-06 14:19:42 -0500

    Update CORS for production

--- src/server.js
+++ src/server.js
@@ -12,7 +12,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://childcare-dashboard-iota.vercel.app/'
+  ]
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
